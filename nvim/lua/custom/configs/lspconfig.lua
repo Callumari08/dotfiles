3293cda8b0e635d7252288f4738b433cd6f43a17
Callum <callum.feeney08@gmail.com>
2024-05-29 19:17:45 +0100

    Updates, Updates Updates

---
+++ nvim/lua/custom/configs/lspconfig.lua
@@ -0,0 +1,54 @@
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+local lspconfig = require("lspconfig")
+local util = require "lspconfig/util"
+local pid = vim.fn.getpid()
+
+lspconfig.rust_analyzer.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = {"rust"},
+  root_dir = util.root_pattern("Cargo.toml"),
+  settings = {
+    cargo = {
+      allFeatures = true,
+    },
+  },
+})
+
+lspconfig.omnisharp.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  cmd = { "dotnet", "/home/cal/.local/share/nvim/mason/packages/omnisharp/libexec/OmniSharp.dll" },
+  enable_editorconfig_support = true,
+  enable_ms_build_load_projects_on_demand = false,
+  enable_roslyn_analyzers = false,
+  organize_imports_on_format = true,
+  enable_import_completion = true,
+  sdk_include_prereleases = true,
+  analyze_open_documents_only = false,
+})
+
+lspconfig.pyright.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = {"python"},
+})
+
+lspconfig.clangd.setup {
+  on_attach = function (client,bufnr)
+    client.server_capabilities.signatureHelpProvider = false
+    on_attach(client, bufnr)
+  end,
+  capabilities = capabilities,
+}
+
+lspconfig.tsserver.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  init_options = {
+    preferences = {
+      disableSuggestions = true
+    }
+  }
+}
